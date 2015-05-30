cd0f33d100aedfae782fba2edce8c300c4671cdf
hemildesai <desai38@purdue.edu>
2015-05-30 15:04:17 +0530

    Added search and a little style to the main layout

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,8 +6,25 @@ class Book < ActiveRecord::Base
   scope :recent, -> {
     where('finished_on > ?', 2.days.ago)
   }
+  scope :search, ->(keyword) {
+    where('keywords LIKE ?', "%#{keyword.downcase}%") if keyword.present?
+  }
+
+  before_save :set_keywords
 
   def finished?
     finished_on.present?
   end
+
+  protected
+  def set_keywords
+    self.keywords = [title, author, description].map(&:downcase).join(" ")
+  end
+  # def self.search(keyword)
+  #   if keyword.present?
+  #     where(title: keyword)
+  #   else
+  #     all
+  #   end
+  # end
 end
